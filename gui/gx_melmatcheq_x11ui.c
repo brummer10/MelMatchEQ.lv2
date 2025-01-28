67b8d60141ddb93341508a61306b8226ebcf9e7f
Autumn Ives <146475002+lpn256@users.noreply.github.com>
2025-01-27 17:18:36 -0800

    Update gx_melmatcheq_x11ui.c

--- gui/gx_melmatcheq_x11ui.c
+++ gui/gx_melmatcheq_x11ui.c
@@ -18,9 +18,8 @@
 #include <X11/Xutil.h>
 #include <X11/keysym.h>
 #include <X11/Xatom.h>
-#include <X11/Xlib.h>
 #include <X11/Xresource.h>
-#include <X11/Xutil.h>
+
 
 #include "./gx_melmatcheq.h"
 
